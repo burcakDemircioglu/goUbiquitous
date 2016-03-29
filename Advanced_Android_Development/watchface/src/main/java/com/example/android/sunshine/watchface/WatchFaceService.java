716e595441e91ddd3053555ae7d55952e6917cd4
burcak <burcakdemircioglu@gmail.com>
2016-03-30 00:36:41 +0300

    feat: Modify the Text Positions and Sizes on Watchface
    feat: Disconnect the listener if the watch face is at ambient mode
    feat: Change font to Roboto (both light end regular)

--- Advanced_Android_Development/watchface/src/main/java/com/example/android/sunshine/watchface/WatchFaceService.java
+++ Advanced_Android_Development/watchface/src/main/java/com/example/android/sunshine/watchface/WatchFaceService.java
@@ -66,9 +66,6 @@ import java.util.concurrent.TimeUnit;
  * low-bit ambient mode, the text is drawn without anti-aliasing in ambient mode.
  */
 public class WatchFaceService extends CanvasWatchFaceService {
-    private static final Typeface NORMAL_TYPEFACE =
-            Typeface.create(Typeface.SANS_SERIF, Typeface.NORMAL);
-
     /**
      * Update rate in milliseconds for interactive mode. We update once a second since seconds are
      * displayed in interactive mode.
@@ -111,12 +108,14 @@ public class WatchFaceService extends CanvasWatchFaceService {
         final Handler mUpdateTimeHandler = new EngineHandler(this);
         boolean mRegisteredTimeZoneReceiver = false;
         Paint mBackgroundPaint;
+
         Paint mHourPaint;
         Paint mMinutePaint;
         Paint mHighPaint;
         Paint mLowPaint;
-        Paint mTextPaint;
         Paint mDatePaint;
+
+        Paint mTextPaint;
         Paint mDateAmbientPaint;
         Paint mbatteryPaint;
 
@@ -171,19 +170,170 @@ public class WatchFaceService extends CanvasWatchFaceService {
 
             mTextPaint = new Paint();
             mTextPaint = createTextPaint(resources.getColor(R.color.digital_text));
-            //mTextPaint.setStrokeWidth(5.0f);
+
+            mHourPaint=new Paint();
+            mHourPaint = createTextPaint(resources.getColor(R.color.digital_text));
+
+            mMinutePaint=new Paint();
+            mMinutePaint = createTextPaint(resources.getColor(R.color.digital_text));
+            mMinutePaint.setTypeface(Typeface.createFromAsset(getResources().getAssets(), "Roboto-Light.ttf"));
+
             mHighPaint = new Paint();
             mHighPaint = createTextPaint(resources.getColor(R.color.watchface_date));
             mHighPaint.setFakeBoldText(true);
-            mDatePaint = new Paint();
-            mDateAmbientPaint = new Paint();
-            mbatteryPaint = new Paint();
 
-            mDateAmbientPaint = createTextPaint(resources.getColor(R.color.digital_text));
+            mLowPaint = new Paint();
+            mLowPaint =createTextPaint(resources.getColor(R.color.watchface_date));
+            mLowPaint.setTypeface(Typeface.createFromAsset(getResources().getAssets(), "Roboto-Light.ttf"));
+
+            mDatePaint = new Paint();
             mDatePaint = createTextPaint(resources.getColor(R.color.watchface_date));
+
+            mDateAmbientPaint = new Paint();
+            mDateAmbientPaint = createTextPaint(resources.getColor(R.color.digital_text));
+
+            mbatteryPaint = new Paint();
             mbatteryPaint = createTextPaint(resources.getColor(R.color.watchface_date));
 
             mTime = new Time();
+        }
+
+        @Override
+        public void onApplyWindowInsets(WindowInsets insets) {
+            super.onApplyWindowInsets(insets);
+
+            // Load resources that have alternate values for round watches.
+            Resources resources = WatchFaceService.this.getResources();
+            boolean isRound = insets.isRound();
+            mXOffset = resources.getDimension(isRound
+                    ? R.dimen.digital_x_offset_round : R.dimen.digital_x_offset);
+            float hourSize = resources.getDimension(isRound
+                    ? R.dimen.digital_text_size_round : R.dimen.digital_text_size);
+            float dateSize = resources.getDimension(isRound
+                    ? R.dimen.digital_text_size_round_2 : R.dimen.digital_text_size_2);
+            float batterySize = resources.getDimension(isRound
+                    ? R.dimen.digital_text_size_round_3 : R.dimen.digital_text_size_3);
+            float tempSize = resources.getDimension(isRound
+                    ? R.dimen.digital_text_size_round_4 : R.dimen.digital_text_size_4);
+
+            mTextPaint.setTextSize(hourSize);
+
+            mHourPaint.setTextSize(hourSize);
+            mMinutePaint.setTextSize(hourSize);
+            mHighPaint.setTextSize(tempSize);
+            mLowPaint.setTextSize(tempSize);
+            mDatePaint.setTextSize(dateSize);
+
+            mDateAmbientPaint.setTextSize(dateSize);
+            mbatteryPaint.setTextSize(batterySize);
+        }
+
+        @Override
+        public void onDraw(Canvas canvas, Rect bounds) {
+
+            IntentFilter iFilter = new IntentFilter(Intent.ACTION_BATTERY_CHANGED);
+            Intent batteryStatus =  getApplicationContext().registerReceiver(null, iFilter);
+            int batteryLevel = batteryStatus.getIntExtra(BatteryManager.EXTRA_LEVEL, -1);
+
+            // Draw the background.
+            if (isInAmbientMode()) {
+                canvas.drawColor(Color.BLACK);
+            } else {
+                canvas.drawRect(0, 0, bounds.width(), bounds.height(), mBackgroundPaint);
+            }
+
+            // Draw H:MM in ambient mode or H:MM:SS in interactive mode.
+            mTime.setToNow();
+            String day=getDayWord(mTime.weekDay);
+            String month=getMonthWord(mTime.month);
+            mdateString=day+", "+month+" "+mTime.monthDay+" "+mTime.year;
+            String text = String.format("%d:%02d", mTime.hour, mTime.minute);
+            String hour = String.format("%d", mTime.hour);
+            String minute = String.format(":%02d", mTime.minute);
+
+            String textDate = !mAmbient
+                    ? String.format("%d.%02d.%02d", mTime.monthDay, mTime.month + 1, mTime.year % 100)
+                    : String.format("%d.%02d", mTime.monthDay, mTime.month + 1);
+            String highString=high+(char) 0x00B0 ;
+            String lowString=low+(char) 0x00B0 ;
+            String battery=Integer.toString(batteryLevel)+"%";
+            //scale bitmap
+            int scale = 70;
+            weatherIcon = Bitmap.createScaledBitmap(weatherIcon, scale, scale, true);
+
+
+            if (!mAmbient) {
+                canvas.drawText(battery, bounds.centerX() - mbatteryPaint.measureText(battery) / 2, mYOffset / 3, mbatteryPaint);
+
+                canvas.drawText(hour, bounds.centerX() - (mHourPaint.measureText(text)) / 2, mYOffset, mHourPaint);
+                canvas.drawText(minute, bounds.centerX() - (mHourPaint.measureText(text)) / 2 + mHourPaint.measureText(hour), mYOffset, mMinutePaint);
+
+                canvas.drawText(mdateString, bounds.centerX() - (mDatePaint.measureText(mdateString)) / 2, mYOffset + 50, mDatePaint);
+
+                canvas.drawLine(bounds.centerX() - (mTextPaint.measureText(text)) / 6, mYOffset + 80,
+                        mTextPaint.measureText(text) / 3 + bounds.centerX() - (mTextPaint.measureText(text)) / 6, mYOffset + 80, mDatePaint);
+
+                canvas.drawText(highString, bounds.centerX()-mHighPaint.measureText(high)/2, mYOffset + 150, mHighPaint);
+                canvas.drawText(lowString,bounds.centerX() + mHighPaint.measureText(highString)/2+20, mYOffset + 150, mLowPaint);
+
+                canvas.drawBitmap(weatherIcon, bounds.centerX() - mHighPaint.measureText(highString)/2-90, mYOffset + 100, mDatePaint);
+            }
+            else{
+                canvas.drawText(text, bounds.centerX() - (mTextPaint.measureText(text)) / 2, mYOffsetAmbient, mTextPaint);
+                canvas.drawText(textDate, bounds.centerX() - (mDatePaint.measureText(textDate)) / 2, mYOffsetAmbient + 70, mDateAmbientPaint);
+            }
+        }
+
+        @Override
+        public void onPropertiesChanged(Bundle properties) {
+            super.onPropertiesChanged(properties);
+            mLowBitAmbient = properties.getBoolean(PROPERTY_LOW_BIT_AMBIENT, false);
+        }
+
+        @Override
+        public void onTimeTick() {
+            super.onTimeTick();
+            invalidate();
+        }
+
+        @Override
+        public void onAmbientModeChanged(boolean inAmbientMode) {
+            super.onAmbientModeChanged(inAmbientMode);
+            if (mAmbient != inAmbientMode) {
+                mAmbient = inAmbientMode;
+                if (mLowBitAmbient) {
+                    mTextPaint.setAntiAlias(!inAmbientMode);
+                }
+                invalidate();
+            }
+
+            // Whether the timer should be running depends on whether we're visible (as well as
+            // whether we're in ambient mode), so we may need to start or stop the timer.
+            updateTimer();
+        }
+
+        /**
+         * Captures tap event (and tap type) and toggles the background color if the user finishes
+         * a tap.
+         */
+        @Override
+        public void onTapCommand(int tapType, int x, int y, long eventTime) {
+            Resources resources = WatchFaceService.this.getResources();
+            switch (tapType) {
+                case TAP_TYPE_TOUCH:
+                    // The user has started touching the screen.
+                    break;
+                case TAP_TYPE_TOUCH_CANCEL:
+                    // The user has started a different gesture or otherwise cancelled the tap.
+                    break;
+                case TAP_TYPE_TAP:
+                    // The user has completed the tap gesture.
+                    mTapCount++;
+                    mBackgroundPaint.setColor(resources.getColor(mTapCount % 2 == 0 ?
+                            R.color.background : R.color.background2));
+                    break;
+            }
+            invalidate();
         }
 
         @Override
@@ -295,8 +445,11 @@ public class WatchFaceService extends CanvasWatchFaceService {
         private Paint createTextPaint(int textColor) {
             Paint paint = new Paint();
             paint.setColor(textColor);
-            paint.setTypeface(NORMAL_TYPEFACE);
+            paint.setTypeface(Typeface.createFromAsset(getResources().getAssets(), "Roboto-Regular.ttf"));
             paint.setAntiAlias(true);
+            //paint.setDither(true);
+            //paint.setStyle(Paint.Style.STROKE);
+            //paint.setStrokeWidth(1);
             return paint;
         }
 
@@ -384,11 +537,12 @@ public class WatchFaceService extends CanvasWatchFaceService {
                 mTime.clear(TimeZone.getDefault().getID());
                 mTime.setToNow();
                 mGoogleApiClient.connect();
+                Wearable.DataApi.addListener(mGoogleApiClient, this);
 
             } else {
                 unregisterReceiver();
-                //Wearable.DataApi.removeListener(mGoogleApiClient, this);
-                //mGoogleApiClient.disconnect();
+                Wearable.DataApi.removeListener(mGoogleApiClient, this);
+                mGoogleApiClient.disconnect();
             }
 
             // Whether the timer should be running depends on whether we're visible (as well as
@@ -412,128 +566,6 @@ public class WatchFaceService extends CanvasWatchFaceService {
             mRegisteredTimeZoneReceiver = false;
             WatchFaceService.this.unregisterReceiver(mTimeZoneReceiver);
         }
-
-        @Override
-        public void onApplyWindowInsets(WindowInsets insets) {
-            super.onApplyWindowInsets(insets);
-
-            // Load resources that have alternate values for round watches.
-            Resources resources = WatchFaceService.this.getResources();
-            boolean isRound = insets.isRound();
-            mXOffset = resources.getDimension(isRound
-                    ? R.dimen.digital_x_offset_round : R.dimen.digital_x_offset);
-            float textSize = resources.getDimension(isRound
-                    ? R.dimen.digital_text_size_round : R.dimen.digital_text_size);
-            float textSize2 = resources.getDimension(isRound
-                    ? R.dimen.digital_text_size_round_2 : R.dimen.digital_text_size_2);
-            float textSize3 = resources.getDimension(isRound
-                    ? R.dimen.digital_text_size_round_3 : R.dimen.digital_text_size_3);
-            mTextPaint.setTextSize(textSize);
-            mHighPaint.setTextSize(textSize2);
-            mDatePaint.setTextSize(textSize2);
-            mDateAmbientPaint.setTextSize(textSize2);
-            mbatteryPaint.setTextSize(textSize3);
-
-
-        }
-
-        @Override
-        public void onPropertiesChanged(Bundle properties) {
-            super.onPropertiesChanged(properties);
-            mLowBitAmbient = properties.getBoolean(PROPERTY_LOW_BIT_AMBIENT, false);
-        }
-
-        @Override
-        public void onTimeTick() {
-            super.onTimeTick();
-            invalidate();
-        }
-
-        @Override
-        public void onAmbientModeChanged(boolean inAmbientMode) {
-            super.onAmbientModeChanged(inAmbientMode);
-            if (mAmbient != inAmbientMode) {
-                mAmbient = inAmbientMode;
-                if (mLowBitAmbient) {
-                    mTextPaint.setAntiAlias(!inAmbientMode);
-                }
-                invalidate();
-            }
-
-            // Whether the timer should be running depends on whether we're visible (as well as
-            // whether we're in ambient mode), so we may need to start or stop the timer.
-            updateTimer();
-        }
-
-        /**
-         * Captures tap event (and tap type) and toggles the background color if the user finishes
-         * a tap.
-         */
-        @Override
-        public void onTapCommand(int tapType, int x, int y, long eventTime) {
-            Resources resources = WatchFaceService.this.getResources();
-            switch (tapType) {
-                case TAP_TYPE_TOUCH:
-                    // The user has started touching the screen.
-                    break;
-                case TAP_TYPE_TOUCH_CANCEL:
-                    // The user has started a different gesture or otherwise cancelled the tap.
-                    break;
-                case TAP_TYPE_TAP:
-                    // The user has completed the tap gesture.
-                    mTapCount++;
-                    mBackgroundPaint.setColor(resources.getColor(mTapCount % 2 == 0 ?
-                            R.color.background : R.color.background2));
-                    break;
-            }
-            invalidate();
-        }
-
-        @Override
-        public void onDraw(Canvas canvas, Rect bounds) {
-
-            IntentFilter iFilter = new IntentFilter(Intent.ACTION_BATTERY_CHANGED);
-            Intent batteryStatus =  getApplicationContext().registerReceiver(null, iFilter);
-            int batteryLevel = batteryStatus.getIntExtra(BatteryManager.EXTRA_LEVEL, -1);
-
-            // Draw the background.
-            if (isInAmbientMode()) {
-                canvas.drawColor(Color.BLACK);
-            } else {
-                canvas.drawRect(0, 0, bounds.width(), bounds.height(), mBackgroundPaint);
-            }
-
-            // Draw H:MM in ambient mode or H:MM:SS in interactive mode.
-            mTime.setToNow();
-            String day=getDayWord(mTime.weekDay);
-            String month=getMonthWord(mTime.month);
-            mdateString=day+", "+month+" "+mTime.monthDay+" "+mTime.year;
-            String text = String.format("%d:%02d", mTime.hour, mTime.minute);
-            String textDate = !mAmbient
-                    ? String.format("%d.%02d.%02d", mTime.monthDay, mTime.month + 1, mTime.year % 100)
-                    : String.format("%d.%02d", mTime.monthDay, mTime.month + 1);
-            String highString=high+(char) 0x00B0 ;
-            String lowString=low+(char) 0x00B0 ;
-            String battery=Integer.toString(batteryLevel)+"%";
-            //scale bitmap
-            int scale = 70;
-            weatherIcon = Bitmap.createScaledBitmap(weatherIcon, scale, scale, true);
-            if (!mAmbient) {
-                canvas.drawText(text, bounds.centerX() - (mTextPaint.measureText(text)) / 2, mYOffset, mTextPaint);
-                canvas.drawText(mdateString, bounds.centerX() - (mDatePaint.measureText(mdateString)) / 2, mYOffset + 50, mDatePaint);
-                canvas.drawLine(bounds.centerX() - (mTextPaint.measureText(text)) / 4, mYOffset + 90,
-                        mTextPaint.measureText(text) / 2 + bounds.centerX() - (mTextPaint.measureText(text)) / 4, mYOffset + 90, mDatePaint);
-                canvas.drawText(highString, bounds.centerX(), mYOffset + 150, mHighPaint);
-                canvas.drawText(lowString,bounds.centerX() + mDatePaint.measureText(highString)+15, mYOffset + 150, mDatePaint);
-                canvas.drawText(battery, bounds.centerX() - mbatteryPaint.measureText(battery) / 2, mYOffset / 3, mbatteryPaint);
-                canvas.drawBitmap(weatherIcon, bounds.centerX() - (mTextPaint.measureText(text)) / 2-10, mYOffset + 100, mDatePaint);
-            }
-            else{
-                canvas.drawText(text, bounds.centerX() - (mTextPaint.measureText(text)) / 2, mYOffsetAmbient, mTextPaint);
-                canvas.drawText(textDate, bounds.centerX() - (mDatePaint.measureText(textDate)) / 2, mYOffsetAmbient + 70, mDateAmbientPaint);
-            }
-        }
-
         /**
          * Starts the {@link #mUpdateTimeHandler} timer if it should be running and isn't currently
          * or stops it if it shouldn't be running but currently is.
